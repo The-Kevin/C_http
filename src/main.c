357cb13b38a8eac19297025be0f17d5d0faf7cda
Kevin Almeida <kevin.almeida1111.ka@gmail.com>
2024-01-27 20:29:40 +0545

    feat: create addressinfo

--- src/main.c
+++ src/main.c
@@ -2,22 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#define DEBUG 1
 
 #define MAX 80
 #define PORT 80
-#define LIFE true
+#define LIFE 1
 
-void error(const char *msg) { perror(msg); exit(0); }
+void error(const char *msg) { perror(msg); exit(1); }
+void debugLog(const char *msg) { if(DEBUG) printf("%s", msg); }
+
 
 int main(int argc, char *argv[]){
 	
 	int sockfd;
+	struct sockaddr_in address;
 
-	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	sockfd = socket(AF_UNSPEC,SOCK_STREAM,0);
 
 	if(sockfd == -1) error("Socket creating falied");
-		
+	debugLog("Socket was created");
 	
+	bzero(&address, sizeof(address));
+	
+	
+	address.sin_family = AF_UNSPEC; //whatever, IPV4 or IPV6
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
+	address.sin_port = htons(PORT);
+	
+
+
+
 	
 
 	return 0;
